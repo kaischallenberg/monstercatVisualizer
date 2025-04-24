1aedb82f50978a3d753fed1eeb18d87f4fab1ed5
Kai Schallenberg <hello@kaischallenberg.de>
2025-04-24 15:03:18 +0200

    New Freq Scale + Improvements to Higher Freqs

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ let first = false
 let container = document.getElementById("visualizer");
 
 let powScale = 5
-let smoothWidth = 6
-let smoothDivi = 7
+let smoothWidth = 10
+let smoothDivi = 8
 let frameRate = 120
 let cssSmooth = true
+let highDivi = 0.1
 
 if (cssSmooth && frameRate >= 60) {
     frameRate = 60
@@ -39,18 +40,21 @@ const audioMotion = new AudioMotionAnalyzer(
             let tempBars = instance.getBars()
 
             // generate array of length with log scale (32 points) starting from 0 and only whole numbers (max of tempBars.length == 1160) with a larger width on the first 15 points
-            let barPoints = [0, 1, 2, 3, 5, 7, 9, 12, 16, 21, 28, 35, 44, 55, 69, 86, 107, 133, 166, 221, 273, 326, 401, 471, 548, 618, 689, 774, 851, 927, 1001, 1078]
+            //let barPoints = [0, 1, 2, 3, 5, 7, 9, 12, 16, 21, 28, 35, 44, 55, 69, 86, 107, 133, 166, 221, 273, 326, 401, 471, 548, 618, 689, 774, 851, 927, 1001, 1078]
+            let barPoints = [0, 1, 2, 3, 4, 6, 8, 10, 13, 18, 23, 29, 37, 46, 58, 72, 89, 111, 139, 184, 228, 272, 334, 393, 457, 516, 574, 646, 709, 774, 836, 900]
 
             for (let x = 0; x < barPoints.length; x++) {
 
-                //let percentage = x / (barPoints.length)
+                let percentage = x / (barPoints.length)
                 // flip the percentage
-                //percentage = Math.pow(percentage, powScale / 3)
-                //percentage = 1 - percentage
+                percentage = Math.pow(percentage, powScale / 7)
+                percentage = 1 - percentage
+                if(percentage < highDivi) percentage = highDivi
+ 
+                if (x < 63 && (barPoints[x + 1] - barPoints[x]) > 10) {
+                    let max = 0
+                    for (let y = barPoints[x]; y < (barPoints[x] + barPoints[x + 1] / 2) - ((barPoints[x + 1] - barPoints[x]) / 1.5); y++) {
 
-                if (x < 63 && barPoints[x] - barPoints[x + 1] > 1) {
-                    max = 0
-                    for (let y = barPoints[x]; y < (barPoints[x] + barPoints[x + 1] / 2); y++) {
                         if (tempBars[y] && tempBars[y].value[0] > max) {
                             max = tempBars[y].value[0]
                         }
@@ -62,6 +66,7 @@ const audioMotion = new AudioMotionAnalyzer(
             }
 
             for (let x = 0; x < barPoints.length; x++) {
+
                 for (let y = 0; y < smoothWidth; y++) {
                     if (barPoints[x + y] !== undefined && barPoints[x] > barPoints[x + y]) {
                         barPoints[x + y] += barPoints[x] / (smoothDivi * y)
